b47b78ee7d939a86030a3d2612dc8bc1edb92328
Abdul Alim <aa.abdulalim13@gmail.com>
2023-10-25 00:10:43 +0600

    update admin functionality

---
+++ app/api/admin/categories/route.ts
@@ -0,0 +1,11 @@
+import { getAll, ValidModelNames } from "@/app/api/services/getAll";
+import { NextRequest } from "next/server";
+
+// Define the model
+const model: ValidModelNames = "category";
+
+// GET CATEGORY
+export async function GET(req: NextRequest) {
+  // Call the 'getAll' function to retrieve all Items
+  return getAll(model);
+}
--- app/api/services/getAll.ts
+++ app/api/services/getAll.ts
@@ -7,41 +7,32 @@ const prisma = new PrismaClient();
 export type ValidModelNames = "category" | "product" | "user";
 
 // Map model names to their corresponding Prisma methods
-const modelMethods: Record<ValidModelNames, (args: any) => Promise<any>> = {
-  category: (args) => prisma.category.findUnique(args),
-  product: (args) => prisma.product.findUnique(args),
-  user: (args) => prisma.user.findUnique(args),
+const modelMethods: Record<ValidModelNames, () => Promise<any>> = {
+  category: () => prisma.category.findMany(),
+  product: () => prisma.product.findMany(),
+  user: () => prisma.user.findMany(),
 };
 
-export async function getAll(item: ValidModelNames, id: string) {
+export async function getAll(item: ValidModelNames) {
   try {
-    // Check if the 'id' is missing
-    if (!id) {
-      return sendResponse(400, false, { message: `${item} ID is missing` });
-    }
-
-    // Retrieve the appropriate 'findUnique' method based on 'item'
-    const findUnique = modelMethods[item];
+    // Retrieve the appropriate 'findManay' method based on 'item'
+    const findMany = modelMethods[item];
 
     // Check if the 'item' is not found in 'modelMethods'
-    if (!findUnique) {
+    if (!findMany) {
       return sendResponse(404, false, { message: `${item} table not found` });
     }
 
-    // Find and return the unique item using the selected 'findUnique' method
-    const foundItem = await findUnique({
-      where: {
-        id: parseInt(id),
-      },
-    });
+    // Find and return the unique item using the selected 'findManay' method
+    const foundItems = await findMany();
 
     // Check if the item is not found
-    if (!foundItem) {
+    if (!foundItems) {
       return sendResponse(404, false, { message: `${item} not found` });
     }
 
     // Send a successful response with the found item
-    return sendResponse(200, true, foundItem);
+    return sendResponse(200, true, foundItems);
   } catch (error) {
     // Log and handle any errors that occur
     console.log(error);
