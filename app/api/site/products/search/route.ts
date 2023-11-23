3c04b0413ef2b46372352e2096a64780cb5d06a0
Abdul Alim <aa.abdulalim13@gmail.com>
2023-11-23 17:21:48 +0600

    updated product search api

--- app/api/site/products/search/route.ts
+++ app/api/site/products/search/route.ts
@@ -3,17 +3,21 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
-  const categories = searchParams.getAll('categories').flatMap(category => category.split(',').map(Number)).filter(Boolean);
-  const ratings = searchParams.getAll('ratings').flatMap(rating => rating.split(',')).map(Number).filter(Boolean);
-  const minPriceParam = searchParams.get('minPrice');
-  const maxPriceParam = searchParams.get('maxPrice');
+  const categories = searchParams
+    .getAll("categories")
+    .flatMap((category) => category.split(",").map(Number))
+    .filter(Boolean);
+  const ratings = searchParams
+    .getAll("ratings")
+    .flatMap((rating) => rating.split(","))
+    .map(Number)
+    .filter(Boolean);
+  const minPriceParam = searchParams.get("minPrice");
+  const maxPriceParam = searchParams.get("maxPrice");
   const minPrice = minPriceParam ? parseFloat(minPriceParam) : undefined;
   const maxPrice = maxPriceParam ? parseFloat(maxPriceParam) : undefined;
-  const page = parseInt(searchParams.get('page') || '1', 10);
-  const pageSize = parseInt(searchParams.get('pageSize') || '10', 10);
-
-  console.log("categories: ", categories);
-  console.log("ratings: ", ratings);
+  const page = parseInt(searchParams.get("page") || "1", 10);
+  const pageSize = parseInt(searchParams.get("pageSize") || "10", 10);
 
   try {
     const products = await prisma.product.findMany({
@@ -22,7 +26,10 @@ export async function GET(req: NextRequest) {
           gte: minPrice,
           lte: maxPrice,
         },
-        reviews: ratings.length > 0 ? { every: { OR: ratings.map(rating => ({ rating })) } } : undefined,
+        reviews:
+          ratings.length > 0
+            ? { every: { OR: ratings.map((rating) => ({ rating })) } }
+            : undefined,
         categoryId: categories.length > 0 ? { in: categories } : undefined,
       },
       take: pageSize,
