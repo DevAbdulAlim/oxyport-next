246ecc5cc5849f4ca1019c653a0a7bdc2852b093
Abdul Alim <aa.abdulalim13@gmail.com>
2024-02-22 09:05:15 +0600

    update not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,10 +2,14 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h2 className="text-3xl font-bold mb-4">Not Found</h2>
+      <p className="text-lg text-gray-600 mb-8">
+        Could not find requested resource
+      </p>
+      <Link href="/">
+        <a className="text-blue-500 hover:underline">Return Home</a>
+      </Link>
     </div>
   );
 }
