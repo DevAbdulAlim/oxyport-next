511a0f84aa45a06b9ad5bc3151fe3aaf4aa78b2b
Abdul Alim <aa.abdulalim13@gmail.com>
2024-03-28 09:30:00 +0600

    updated not found design

---
+++ app/(page)/admin/loading.tsx
@@ -0,0 +1,6 @@
+import Loader from "@/components/loader";
+
+export default function Loading() {
+  // Or a custom loading skeleton component
+  return <Loader />;
+}
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,22 @@
 import Link from "@/components/ui/link";
+import { AiOutlineArrowLeft, AiOutlineExclamationCircle } from "react-icons/ai"; // Importing the required icons
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link to="/">Return Home</Link>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <AiOutlineExclamationCircle className="text-red-500 w-12 h-12 mb-4" />{" "}
+      {/* Adding the Not Found icon */}
+      <h2 className="text-3xl font-bold mb-4">Not Found</h2>
+      <p className="text-gray-600 mb-8">
+        Could not find the requested resource
+      </p>
+      <Link
+        to="/"
+        className="flex items-center gap-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300 ease-in-out"
+      >
+        <AiOutlineArrowLeft className="w-6 h-6" />
+        Return Home
+      </Link>
     </div>
   );
 }
